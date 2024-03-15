d90c01ab629215f84cf39854c39c2d20b3251d6e
kleber89 <benkosisackson@gmail.com>
2024-03-15 17:18:30 -0500

    Malloc, free correction

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -9,17 +9,6 @@
  * Return: NULL in case of error, pointer to allocated
  * space
  *
- * FUNCTIONALITY
- *
- * 1. First, we check if the string is NULL.  If it is, we return NULL.
- * 2.  Next, we loop through the string to find the length of the string.
- * 3.  We allocate memory for the copy of the string.
- * 4.  We check if the memory was allocated.  If it wasn’t, we return NULL.
- * 5.  We loop through the string again, this time copying the string into the new memory.
- * 6.  We add a null terminator to the end of the string.
- * 7.  We return the copy of the string.
- *
- * Time Complexity: O(n)
  */
 char *_strdup(char *str)
 {
