87ff4aeba90902cd5887d1f991b5ffd61966895e
kleber89 <benkosisackson@gmail.com>
2024-04-09 20:50:07 +0000

    prints the binary representation

--- bit_manipulation/1-print_binary.c
+++ bit_manipulation/1-print_binary.c
@@ -1,19 +1,20 @@
 #include "main.h"
 
 /**
- * print_binary - function with one argument
- * @n: decimal value
- *
- * Description: prints the binary representation of a number
- * Return: na
+ * print_binary -  prints the binary representation of a number
+ * @n: integer to convert
  */
 void print_binary(unsigned long int n)
 {
-	unsigned long value = n;
+	int i, flag;
 
-	if (value > 1)
+	if (n == 0)
+		_putchar('0');
+	for (flag = 0, i = sizeof(n) * 8 - 1; i >= 0; i--)
 	{
-		print_binary(value >> 1);
+		if ((n >> i) & 1)
+			flag = 1;
+		if (flag == 1)
+			((n >> i) & 1) ? _putchar('1') : _putchar('0');
 	}
-	_putchar((value & 1) + '0');
 }
